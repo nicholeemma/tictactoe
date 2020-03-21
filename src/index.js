3dbf6afa8fde514f789e2d69e4b4abd1c6d76fa8
jiayuespiraldata <jiayue@spiraldatagroup.com.au>
2020-03-21 12:53:24 +1030

    class component

--- src/index.js
+++ src/index.js
@@ -16,16 +16,33 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square"
-              onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+              onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       squares: Array(9).fill(null)
+    }
+  }
+  
+  handleClick(i) {
+    // create a copy of the squares array
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} 
+                  onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
