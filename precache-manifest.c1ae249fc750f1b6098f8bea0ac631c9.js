7b2f33c428953efe0b690a4668fa1e3428a7c88e
nicholeemma <790956043@qq.com>
2020-03-28 12:34:41 +1030

    Updates

---
+++ precache-manifest.c1ae249fc750f1b6098f8bea0ac631c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2f374f6523916e7bbe96082ccd6b2b",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "a453a6550b30e0fb793c",
+    "url": "/tictactoe/static/css/main.b585d575.chunk.css"
+  },
+  {
+    "revision": "776b002bfbe2917b8f4a",
+    "url": "/tictactoe/static/js/2.340bf7f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.340bf7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a453a6550b30e0fb793c",
+    "url": "/tictactoe/static/js/main.d957bc31.chunk.js"
+  },
+  {
+    "revision": "6be0e21d5e7b0cb56883",
+    "url": "/tictactoe/static/js/runtime-main.41512def.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
